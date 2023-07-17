e2e50eac59ee262c27cfd3c4a11d52960d560b0f
eleron7 <eleron5603@gmail.com>
2023-07-17 10:25:03 +0900

    조건에 맞는 도서 리스트 출력하기

---
+++ lv1/144853.sql
@@ -0,0 +1,6 @@
+-- 조건에 맞는 도서 리스트 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144853
+SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+FROM BOOK
+WHERE CATEGORY = '인문' AND PUBLISHED_DATE BETWEEN '2021-01-01' AND '2021-12-31'
+ORDER BY PUBLISHED_DATE ASC
